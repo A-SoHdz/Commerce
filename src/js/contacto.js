f09761313053fef2edad64e5470e64982a9a4e21
Carlos Trejo <carlostrejo182@gmail.com>
2021-11-04 18:45:34 -0600

    Update contact.html and contacto.js

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -1,6 +1,7 @@
 
 /* traigo el form por su id */
- let form = document.getElementById("contact-form");
+let form = document.getElementById("contact-form");
+
 
 
 form.addEventListener("submit",function(e){
@@ -10,50 +11,73 @@ form.addEventListener("submit",function(e){
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone");
     let message = document.getElementById("message");
-
-    console.log("enviado");
-
-    Email.send({
-        SecureToken : "fc1e6f88-3a36-4dd6-b47d-378c1960db2f",
-        To : "rockietess@gmail.com",
-        From : "rockietess@gmail.com",
-        Subject : "This is the subject",
-        Body : `Hola soy ${nme} mi correo es ${email} \n ${message} \n mi telefono es ${phone}`
-    }).then(
-      message => alert(message)
-    );
-
-    /* validacion del campo nombre */
-    if(nme.value.length == 0){
-        
-    alert("No puede estar el campo nombre vacio");
-    }
-
-    /* validación del campo email */
-    if(email.length <= 8){
-        alert("Error en el campo email")
-        console.log(`La longitud de tu email es incorrecta ${email}`)
-    }
-
+    
     /* parseamos el dato phone a number */
     let phone_number=parseInt(phone.value,10);
 
-    /* validaciones del campo telefono */
-    if(phone.value.length<10 || phone_number <= 0 || phone.value.length >=11){
-        phone.classList.add("is-invalid");
-        alert("Número de teléfono erróneo, verifica tu número.")
-    }else if(isNaN(phone.value)){
-        phone.classList.add("is-invalid");
-        alert("No ingresar palabras en el campo de teléfono.")
+    if(nme.value.length == 0){/* validacion del campo nombre */
+        
+    alert("No puede estar el campo nombre vacio");
+
     }else{
-        phone.classList.remove("is-invalid");
-        phone.classList.add("is-valid");
-        console.log("número telefonico correcto");
-    }
 
-    if(message.value.length == 0 || message.value.length <20){
-        alert("No puede estar el campo texto vacío");
+        if(email.length <= 8 ){/* validación del campo email */
+            /* Para validar si el email es corto */
+            alert("No cumple con el mínimo de los caractéres posibles el campo Correo electrónico (mínimo = 8)");
+        }else if(email.length >=40){
+            /* Para validar si el email es demasiado largo*/
+            alert("Excedio el máximo de caractéres posibles el campo Correo electrónico(máximo = 40)");
 
-    }
+        }else{
+            
+
+            if(phone.value.length<10 || phone_number <= 0 || phone.value.length >=11){/* validaciones del campo telefono */
+                phone.classList.add("is-invalid");
+                alert("Número de teléfono erróneo, verifica tu número.");
+            }else if(isNaN(phone.value)){
+                phone.classList.add("is-invalid");
+                alert("No ingresar palabras en el campo de teléfono.");
+            }else{
+                
+
+                if(phone.value.length<10 || phone_number <= 0 || phone.value.length >=11){/* validaciones del campo telefono */
+       
+                    phone.classList.add("is-invalid");
+                    alert("Número de teléfono erróneo, verifica tu número.");
+                }else if(isNaN(phone.value)){
+                    phone.classList.add("is-invalid");
+                    alert("No ingresar palabras en el campo de teléfono.");
+                }else{
+                    
+                    if(message.value.length == 0 || message.value.length <20){
+       
+                        alert("El campo texto necesita al menos 20 caracteres");
+                    }else{
+                        
+                        sendEmail(nme,email,message,phone);
+                    }
+
+                }
+
+            }
+
+        }
+
+    } 
     
 }); 
+
+function sendEmail(nme,email,message,phone){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "carlostrejo182@gmail.com",
+        Password : "CEE868D15EBDAA1830EFDE0D2589DEB74A8B",
+        To : 'carlostrejo182@gmail.com',
+        From : "carlostrejo182@gmail.com",
+        Subject : "This is the subject",
+        Body : `Hola soy ${nme.value} mi correo es ${email.value} \n ${message.value} \n mi telefono es ${phone.value}`
+    }).then(
+      message => alert(`Datos enviados satisfactoriamente.`)
+    );
+
+}
